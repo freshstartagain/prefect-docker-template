808f8240cd0becd6c9df25a9db69df512d761fdc
freshstartagain <newstartagainhays@gmail.com>
2022-12-15 20:33:49 +0800

    feat: test prefect agent and postgres up

--- tests/test_prefect.py
+++ tests/test_prefect.py
@@ -1,11 +1,32 @@
 import requests
+import docker
 import pytest
 from config import PREFECT_ORION_UI_API_URL
 
-@pytest.mark.flaky(retries=10, delay=1)
-def test_server():
 
-    response = requests.get(f'{PREFECT_ORION_UI_API_URL}/health')
+DOCKER_CLIENT = docker.DockerClient(base_url="unix://var/run/docker.sock")
+RETRIES = 10
+DELAY = 1
+
+
+@pytest.mark.flaky(retries=RETRIES, delay=DELAY)
+def test_server():
+    response = requests.get(f"{PREFECT_ORION_UI_API_URL}/health")
 
     assert response.status_code == 200
 
+
+@pytest.mark.flaky(retries=RETRIES, delay=DELAY)
+def test_agent():
+    container = DOCKER_CLIENT.containers.get("prefect-docker-template-prefect-agent-1")
+    container_status = container.attrs["State"]["Status"]
+
+    assert container_status == "running"
+
+
+@pytest.mark.flaky(retries=RETRIES, delay=DELAY)
+def test_postgres():
+    container = DOCKER_CLIENT.containers.get("prefect-docker-template-postgres-1")
+    container_status = container.attrs["State"]["Status"]
+
+    assert container_status == "running"
